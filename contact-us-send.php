eaf64e35778ceca88f374dfb2d13abac4da6e13a
Terrence Whaley <terrencew1995@gmail.com>
2018-07-19 11:59:31 -0500

    Added email functionality with thank you page after submission.

---
+++ contact-us-send.php
@@ -0,0 +1,23 @@
+<?php
+if(isset($_POST['submit'])) {
+
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo = "terrencew1995@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from ".$name."\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: contact-us-thank-you.html");
+  }
+
+
+
+    else{
+      header('Location: 404error.html');
+      exit(0);
+    }
+ ?>
